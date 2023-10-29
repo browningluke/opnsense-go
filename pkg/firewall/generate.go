71586873b9068e824258c69eabbf3ff92d5e3ed6
Luke Browning <browningluke01@gmail.com>
2023-10-28 18:07:43 -0700

    Add firewall schema and generate

---
+++ pkg/firewall/generate.go
@@ -0,0 +1,4 @@
+//go:generate go run ../../internal/generate/api/main.go -controller firewall
+// ONLY generate directives and package declaration! Do not add anything else to this file.
+
+package firewall
