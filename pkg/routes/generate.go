a28ec12c45d0827733d4e5e9085618dfbd7e6d33
Luke Browning <browningluke01@gmail.com>
2023-10-28 18:05:24 -0700

    Add routes schema and generate

---
+++ pkg/routes/generate.go
@@ -0,0 +1,4 @@
+//go:generate go run ../../internal/generate/api/main.go -controller routes
+// ONLY generate directives and package declaration! Do not add anything else to this file.
+
+package routes
