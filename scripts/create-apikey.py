d43379acc9484d1d2af97e3f463b8921619af7d7
Abhinav Tamaskar <abhinav.tamaskar@commure.com>
2025-07-29 22:48:14 -0400

    fmt

--- scripts/create-apikey.py
+++ scripts/create-apikey.py
@@ -16,21 +16,21 @@ def send_qemu_command(command, socket_path, timeout=5):
         with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
             client.settimeout(timeout)
             client.connect(socket_path)
-            client.sendall((json.dumps(command) + '\n').encode('utf-8'))
-            
-            response = b''
+            client.sendall((json.dumps(command) + "\n").encode("utf-8"))
+
+            response = b""
             while True:
                 ready, _, _ = select.select([client], [], [], timeout)
                 if not ready:
                     raise TimeoutError(f"No response received within {timeout} seconds")
-                
+
                 chunk = client.recv(4096)
                 if not chunk:
                     break
                 response += chunk
-                if b'\n' in chunk:
+                if b"\n" in chunk:
                     break
-            return response.decode('utf-8')
+            return response.decode("utf-8")
     except (socket.error, TimeoutError, ConnectionRefusedError) as e:
         return f"Error: {e}"
 
@@ -40,8 +40,8 @@ command_create = {
     "arguments": {
         "path": "/usr/local/bin/opn-apikey",
         "arg": ["-u", "root", "create"],
-        "capture-output": True
-    }
+        "capture-output": True,
+    },
 }
 
 output = send_qemu_command(command_create, QEMU_GA_SOCKET).strip()
@@ -49,19 +49,16 @@ output = send_qemu_command(command_create, QEMU_GA_SOCKET).strip()
 print(f"Command output: '{output}'")
 
 # {"return": {"pid": 1234}}
-pid = json.loads(output).get('return').get('pid')
+pid = json.loads(output).get("return").get("pid")
 
 print(f"Command executed with PID: {pid}")
 
 sleep(5)
 
-command_status = {
-    "execute": "guest-exec-status",
-    "arguments": {"pid": pid}
-}
+command_status = {"execute": "guest-exec-status", "arguments": {"pid": pid}}
 output = send_qemu_command(command_status, QEMU_GA_SOCKET).strip()
 
-outdata = json.loads(output).get('return').get('out-data')  # base64 encoded output
-decoded_outdata = base64.b64decode(outdata).decode('utf-8').strip()
+outdata = json.loads(output).get("return").get("out-data")  # base64 encoded
+decoded_outdata = base64.b64decode(outdata).decode("utf-8").strip()
 
 print(decoded_outdata, file=sys.stderr)
