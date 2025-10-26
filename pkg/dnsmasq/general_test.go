5897eebc64a09481aba6d6dda52cc8258f4f275a
mjaveld <10114152+mjaveld@users.noreply.github.com>
2025-10-26 17:21:37 +0100

    feat: dnsmasq get general settings from endpoint

---
+++ pkg/dnsmasq/general_test.go
@@ -0,0 +1,38 @@
+package dnsmasq
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/browningluke/opnsense-go/pkg/api"
+)
+
+func TestGeneral(t *testing.T) {
+	opnsense_url := os.Getenv("OPNSENSE_URI")
+	opnsense_key := os.Getenv("OPNSENSE_API_KEY")
+	opnsense_secret := os.Getenv("OPNSENSE_API_SECRET")
+
+	api_client := api.NewClient(api.Options{
+		Uri:           opnsense_url,
+		APIKey:        opnsense_key,
+		APISecret:     opnsense_secret,
+		AllowInsecure: true,
+		MaxBackoff:    30,
+		MinBackoff:    1,
+		MaxRetries:    4,
+	})
+
+	controller := Controller{
+		Api: api_client,
+	}
+	ctx := context.Background()
+
+	respGet, err := controller.GeneralSettingsGet(ctx)
+	if err != nil {
+		t.Fatalf("Failed to get general settings: %v", err)
+	}
+	t.Logf("GeneralSettingsGet: %+v", respGet)
+
+	// respSet, err := controller.GeneralSettingsSet(ctx, )
+}
